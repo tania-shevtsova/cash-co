361832faedd8d7d87af394f0f20c5060e730fda7
Tania <taniameoow@gmail.com>
2020-05-25 00:33:00 +0300

    add

--- cash-app/src/components/service/Service.js
+++ cash-app/src/components/service/Service.js
@@ -1,12 +1,61 @@
-import React from 'react';
-import styles from './Service.module.css';
+import React from "react";
+import styles from "./Service.module.css";
 
 const Service = () => {
-    return (
-        <div className={styles.serviceBack}>
-
+  return (
+    <div className={styles.serviceBack}>
+      <div className={styles.serviceWrapper}>
+        <div className={styles.serviceHead}>
+          <h3 className={styles.serviceHeader}>Service Certified</h3>
+          <span className={styles.serviceDesc}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+            sequi facilis at esse, architecto eaque quae unde qui, impedit
+            reiciendis nisi est optio deserunt aut vitae nemo culpa!
+          </span>
         </div>
-    )
-}
+        <div className={styles.serviceImg}>
+          <div className={styles.layer}></div>
+          <span className={styles.serviceImgDesc}>Auto Repair Services</span>
+          <a href="#" className={styles.serviceImgLink}>
+            Book Now
+          </a>
+          <span className={styles.serviceImgLinkIcon}></span>
+        </div>
+        <ul className={styles.serviceList}>
+          <li className={styles.serviceListItem}>
+            <span className={styles.serviceListItemImg}></span>
+            <div className={styles.layerImg}></div>
+            <div className={styles.serviceItemWrap}>
+            <h4 className={styles.serviceItemDesc}>
+              Which Car Service should I choose ?
+            </h4>
+            <span className={styles.serviceItemAbout}>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Voluptatum, tenetur beatae aperiam autem consequatur nisi.
+            </span>
+            <a href="#" className={styles.serviceItemLink}>View Project</a>
+            <span className={styles.serviceImgLinkIconLink}></span>
+            </div>
+          </li>
+          <li>
+            <span className={styles.serviceListItemImg2}></span>
+            <div className={styles.layerImg2}></div>
+            <div className={styles.serviceItemWrap}>
+            <h4 className={styles.serviceItemDesc2}>
+              Tips to make your tyres last longer.
+            </h4>
+            <span className={styles.serviceItemAbout2}>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat
+              mollitia necessitatibus sit maxime corrupti voluptates.
+            </span>
+            <a href="#" className={styles.serviceItemLink2}>View Project</a>
+            <span className={styles.serviceImgLinkIconLink2}></span>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 export default Service;
